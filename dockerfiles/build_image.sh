536d258ecaafc66acfa4f217177d521ef6e1dc7d
okurnitsov <okurnitsov@peernova.com>
2025-02-18 11:13:49 +0300

    revert versions

--- dockerfiles/build_image.sh
+++ dockerfiles/build_image.sh
@@ -4,8 +4,8 @@ set -eux
 
 # Default values
 : "${ci:=false}"
-: "${quantlib_version:=1.37}"
-: "${boost_version:=1.87.0}"
+: "${quantlib_version:=1.36}"
+: "${boost_version:=1.86.0}"
 : "${swig_version:=4.2.0}"
 boost_dir="$(echo "boost_${boost_version//./_}")"
 
